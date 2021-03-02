49f5c5f793ebdee8baba5275b4e1347b5e50613e
b-chae <cbr5964@naver.com>
2021-03-02 19:29:42 +0900

    Users API View : GET, PUT

--- users/serializers.py
+++ users/serializers.py
@@ -2,8 +2,27 @@ from rest_framework import serializers
 from .models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class RelatedUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username", "first_name", "last_name",
+                  "email", "avatar", "superhost",)
+
+
+class ReadUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         exclude = ("password", "groups", "user_permissions",
-                   "last_login", "is_superuser", "is_staff", "is_active", "date_joined", "favs")
+                   "last_login", "is_superuser", "is_staff", "is_active", "date_joined")
+
+
+class WriteUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email"
+        )
